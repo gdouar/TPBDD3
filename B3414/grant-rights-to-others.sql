857e3813ba4d22e7b297f6c375f22723627bf23b
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2017-01-17 01:52:49 +0100

    This is (almost) the end.. ;)

--- B3414/grant-rights-to-others.sql
+++ B3414/grant-rights-to-others.sql
@@ -5,28 +5,28 @@ GRANT select, update, insert, delete on stock_am to zvergne;
 Grant select, update, insert, delete on stock_am to hhamelin;
 Grant select, update, insert, delete on stock_am to cpottiez;
 
--- droits lectures autres sites pour la table employes
+-- toutes les droits autres sites pour la table employes
 
 GRANT select, update, insert, delete on employes to lpoisse; 
 GRANT select, update, insert, delete on employes to zvergne;
 Grant select, update, insert, delete on employes to hhamelin;
 Grant select, update, insert on employes to cpottiez;
 
--- droits lectures autres sites pour la table clients_am
+-- toutes les droits autres sites pour la table clients_am
 
 GRANT select, update, insert, delete on clients_am to lpoisse; 
 GRANT select, update, insert, delete on clients_am to zvergne;
 Grant select, update, insert, delete on clients_am to hhamelin;
 Grant select, update, insert, delete on clients_am to cpottiez;
 
--- droits lectures autres sites pour la table commandes_am
+-- toutes les droits autres sites pour la table commandes_am
 
 GRANT select, update, insert, delete on commandes_am to lpoisse; 
 GRANT select, update, insert, delete on commandes_am to zvergne;
 Grant select, update, insert, delete on commandes_am to hhamelin;
 Grant select, update, insert, delete on commandes_am to cpottiez;
 
--- droits lectures autres sites pour la table details_commandes_am
+-- toutes les droits autres sites pour la table details_commandes_am
 
 GRANT select, update, insert, delete on details_commandes_am to lpoisse; 
 GRANT select, update, insert, delete on details_commandes_am to zvergne;
